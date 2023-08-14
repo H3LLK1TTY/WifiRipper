c79406b20c98f5c523799d1b2a3690d27046016c
Joel Patrick <141442170+H3LLK1TTY@users.noreply.github.com>
2023-08-14 17:13:33 +0530

    Update 2.3.7
    # New Features
    Added Mac Address Changer
    Added a function to Check whether user is root.
    
    Minor Bug Fixes
    Added Global Banner for all menu.

--- WifiRipper.sh
+++ WifiRipper.sh
@@ -27,37 +27,52 @@ show_loading 5
 echo "Loading complete!"
 sleep 2
 clear
-animate_line() {
-    local line="$1"
-    local delay="$2"
-    
-    for ((i=0; i<${#line}; i++)); do
-        echo -ne "${line:$i:1}"
-        sleep "$delay"
-    done
-    echo
-}
-
-clear
-delay=0.0005
-
-animate_line " ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── " $delay
-animate_line " ─██████──────────██████─██████████─██████████████─██████████─████████████████───██████████─██████████████─██████████████─██████████████─████████████████─── " $delay
-animate_line " ─██░░██──────────██░░██─██░░░░░░██─██░░░░░░░░░░██─██░░░░░░██─██░░░░░░░░░░░░██───██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░░░██─── " $delay
-animate_line " ─██░░██──────────██░░██─████░░████─██░░██████████─████░░████─██░░████████░░██───████░░████─██░░██████░░██─██░░██████░░██─██░░██████████─██░░████████░░██─── " $delay
-animate_line " ─██░░██──────────██░░██───██░░██───██░░██───────────██░░██───██░░██────██░░██─────██░░██───██░░██──██░░██─██░░██──██░░██─██░░██─────────██░░██────██░░██─── " $delay
-animate_line " ─██░░██──██████──██░░██───██░░██───██░░██████████───██░░██───██░░████████░░██─────██░░██───██░░██████░░██─██░░██████░░██─██░░██████████─██░░████████░░██─── " $delay
-animate_line " ─██░░██──██░░██──██░░██───██░░██───██░░░░░░░░░░██───██░░██───██░░░░░░░░░░░░██─────██░░██───██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░░░██─── " $delay
-animate_line " ─██░░██──██░░██──██░░██───██░░██───██░░██████████───██░░██───██░░██████░░████─────██░░██───██░░██████████─██░░██████████─██░░██████████─██░░██████░░████─── " $delay
-animate_line " ─██░░██████░░██████░░██───██░░██───██░░██───────────██░░██───██░░██──██░░██───────██░░██───██░░██─────────██░░██─────────██░░██─────────██░░██──██░░██───── " $delay
-animate_line " ─██░░░░░░░░░░░░░░░░░░██─████░░████─██░░██─────────████░░████─██░░██──██░░██████─████░░████─██░░██─────────██░░██─────────██░░██████████─██░░██──██░░██████─ " $delay
-animate_line " ─██░░██████░░██████░░██─██░░░░░░██─██░░██─────────██░░░░░░██─██░░██──██░░░░░░██─██░░░░░░██─██░░██─────────██░░██─────────██░░░░░░░░░░██─██░░██──██░░░░░░██─ " $delay
-animate_line " ─██████──██████──██████─██████████─██████─────────██████████─██████──██████████─██████████─██████─────────██████─────────██████████████─██████──██████████─ " $delay
-animate_line " ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── " $delay
-animate_line "                                                                                                                                         v2.3.1               " $delay
-animate_line "                                                     A Deauthentication tool By H3LLK1TTY                                                                 " $delay
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+chmod +x launcher.sh
+chmod +x Banner.sh
+bash launcher.sh
 sleep 5
 echo " Please wait..."
+
+function show_interfaces() {
+  bash Banner.sh
+  echo "Connected Network Interfaces:"
+  echo "----------------------------"
+  interfaces=($(ip link show | awk '/^[0-9]+:/ {print substr($2, 1, length($2)-1)}' | grep -v lo))
+  for ((i=0; i<${#interfaces[@]}; i++)); do
+    echo "$((i+1)). ${interfaces[i]}"
+  done
+}
+
+function change_mac_address_random() {
+  read -p "Enter the number corresponding to the network interface to change the MAC address: " num
+
+  # Validate the input
+  re='^[0-9]+$'
+  if ! [[ $num =~ $re ]]; then
+    echo "Invalid input. Please enter a valid number corresponding to the interface."
+    return
+  fi
+
+  if (( num < 1 || num > ${#interfaces[@]} )); then
+    echo "Invalid input. Please enter a number within the range of available interfaces."
+    return
+  fi
+
+  selected_interface=${interfaces[$((num-1))]}
+  new_mac=$(printf "%02X:%02X:%02X:%02X:%02X:%02X" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
+
+  sudo ip link set dev $selected_interface down
+  sudo ip link set dev $selected_interface address $new_mac
+  sudo ip link set dev $selected_interface up
+
+  echo "MAC address for $selected_interface changed to $new_mac."
+  echo "Redirecting in 5s"
+}
+
 function deauthenticate() {
     for mac in "${mac_addresses[@]}"; do
         gnome-terminal -- bash -c "aireplay-ng -0 $deauth_packets -a $mac -D $interface; read -p 'Press Enter to close this terminal window...' sudo airmon-ng stop $interface "
@@ -68,6 +83,7 @@ function deauthenticate() {
 function scan_and_deauthenticate() {
     clear
     while true; do
+        bash Banner.sh
         echo "Available wireless network interfaces:"
         echo "0 - Go Back"
         index=1
@@ -151,20 +167,19 @@ function scan_and_deauthenticate() {
                 esac
             done            
             deauthenticate
-
             while true; do
-                read -p "Do you want to Deauthenticate Again (y/n): " repeat
+                read -p "Press Q or X to exit to menu: " repeat
                 case $repeat in
-                    [Yy]* )
+                    [Qq]* )
                         sudo airmon-ng stop $interface
                         clear
                         break ;;
-                    [Nn]* )
+                    [Xx]* )
                         sudo airmon-ng stop $interface
                         clear
                         return ;;
                     * )
-                        echo "Invalid response. Please enter 'y' or 'n'." ;;
+                        echo "Invalid response. Please enter 'Q' or 'X'." ;;
                 esac
             done
         else
@@ -176,10 +191,12 @@ function scan_and_deauthenticate() {
 clear
 
 while true; do
+    bash Banner.sh
     echo "Main Menu:"
     echo "1 - Scan and Deauthenticate"
-    echo "2 - Help"
-    echo "3 - About Tool"
+    echo "2 - MAC Address Changer"
+    echo "3 - Help"
+    echo "4 - About Tool"
     echo "0 - Exit"
     read -p "Enter your choice: " main_choice
 
@@ -188,63 +205,20 @@ while true; do
             scan_and_deauthenticate
             ;;
         2 ) clear
-            echo "    Wi-Fi Interface Selection:
-        The tool will display a list of available wireless interfaces on your system.
-        Select the number corresponding to the desired wireless interface you want to use for scanning and deauthentication.
-
-    Monitor Mode Activation:
-        The script will switch the selected interface to monitor mode (if not already) using airmon-ng.
-        If the interface name changes to wlan0mon (or similar), the script will automatically detect and use it.
-
-    Scanning for Wi-Fi Networks:
-        The tool will use airodump-ng to scan for nearby Wi-Fi networks.
-        This will display a list of Wi-Fi networks, their BSSIDs (MAC addresses), and other information.
-
-    Deauthentication Attack:
-        You will be prompted to enter the number of MAC addresses you want to deauthenticate.
-        For each MAC address, enter the MAC address of the client/device you want to deauthenticate from the target network.
-
-    Custom Deauthentication Packets:
-        You will be asked if you want to set a custom number of deauthentication packets or use the default (0).
-        If you choose custom, enter the desired number of deauthentication packets to send.
-
-    Deauthentication Process:
-        The tool will start the deauthentication process using aireplay-ng for each provided MAC address.
-        A new terminal window will open for each deauthentication attempt.
-        Once deauthentication is complete, you can press Enter to close the terminal.
-
-    Repeating the Process:
-        After deauthenticating the selected MAC addresses, you will be asked if you want to deauthenticate again.
-        If you choose "y," the tool will repeat the process for another round of deauthentication.
-
-    Exiting the Tool:
-
-    To exit the tool, choose "n" when prompted to repeat the deauthentication process.
-    The script will stop monitor mode using airmon-ng and exit."
+            show_interfaces
+            change_mac_address_random
+            sleep 5
+            clear
             ;;
         3 ) clear
-            echo "Description:
-
-WifiRipper is a powerful shell script designed to automate the process of scanning nearby Wi-Fi networks and performing deauthentication attacks on multiple networks. This script is intended for educational and testing purposes only and should be used responsibly and with proper authorization of network admins.
-
-Features:
-
-Interface Selection: The script allows the user to choose a wireless network interface from the available options on their system. This ensures that the user is targeting the correct wireless interface for network scanning and deauthentication.
-
-Network Scanning: WifiRipper initiates network scanning using the popular tool airodump-ng, which captures information about nearby Wi-Fi networks. The script displays a list of available networks along with their details, including BSSID, SSID, channel, encryption type, and signal strength.
-
-MAC Address Selection: After scanning, the user is prompted to enter the number of MAC addresses they want to target for deauthentication. The user can then provide the MAC addresses of the specific Wi-Fi networks they wish to deauthenticate.
-
-Deauthentication Attack: WifiRipper launches multiple terminal windows, each executing the aireplay-ng command to perform deauthentication attacks on the selected MAC addresses. 
-Deauthentication attacks are simulated disassociation packets sent to the target network's clients, causing temporary network disruption.
-
-Repeat Option: Once the deauthentication attacks are completed, the user is given the option to repeat the process. This allows for continuous testing and monitoring of Wi-Fi network security.
-
-Dependency Check: Wifiripper checks if the required tools (airodump-ng and aireplay-ng) are installed on the system. If not, it offers to install them using the package manager.
-
-Disclaimer:
-
-WifiRipper is intended for ethical and educational purposes only. Unauthorized use of this script to disrupt Wi-Fi networks without proper authorization is illegal and unethical. Users are encouraged to obtain the necessary permissions and follow ethical guidelines before using this script. The script authors and maintainers are not responsible for any misuse or harm caused by its usage."
+            cat < Manual.md
+            sleep 5
+            clear
+            ;;
+        4 ) clear
+            cat < README.md
+            sleep 5
+            clear
             ;;    
         0 )
             clear
